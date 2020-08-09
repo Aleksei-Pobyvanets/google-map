af4189b3b32077451c154a387f5b91d185895f40
Aleksei <kilroygood@gmail.com>
2020-08-09 16:03:20 +0300

    remove

--- script.js
+++ script.js
@@ -1,54 +1,89 @@
-  
 document.addEventListener("DOMContentLoaded", function (event) {
 console.log("DOM fully loaded and parsed");
 
-    let map;
-    let markers = [];
+    var markers = [];
+    const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let labelIndex = 0;
+
+
+    console.log(markers)
+
+    function initialize() {
+        var mapOptions = {
+            zoom: 18,
+            center: new google.maps.LatLng(38.983064, -76.9473872) 
+        };
     
-    function alrt(){
-        var value = prompt("Please enter your text");
-        if (value == null || value == "") {
-        alert("Your have added marker with out text"),alrt();
-        } else {
-        alert("All right") ;
+        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    
+        markers[0] = new google.maps.Marker({
+            position: new google.maps.LatLng(38.983064, -76.9473872),
+            map: map,
+            label: labels[labelIndex++ % labels.length],
+            title: "Van Munching Hall"
+        });
+    
+        markers[1]=new google.maps.Marker({
+            position: new google.maps.LatLng(38.9827089, -76.9475412),
+            map: map,
+            label: labels[labelIndex++ % labels.length],
+            title: "The Smith Store",
+        });
+    
+        function setMapOnAll() {
+          for (let i = 1; i < markers.length; i++) {
+            markers[i].setMap();
+          }
         }
-    console.log(value)
-    }
 
+        for (var i = 0; i < markers.length; i++) {
+          
+          var mark = markers[i]
+          console.log(mark);
+        }
+        
+        google.maps.event.addListener(mark, 'click', removeMar) 
+          
+        
+        function removeMar() {
+          setMapOnAll(mark)
+        };
 
-    // function showTitel(value) {
-    //     alert(value)
-    // }
-
-    function initMap() {
-      const haightAshbury = { lat: 48.9862071, lng: 31.3835894 };
-      map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 6,
-        center: haightAshbury,
-    });
-    new google.maps.Marker({
-        map,
-        title: "Hello World!"
-    });
-      
-      map.addListener("click", event => {
-        alrt()
-        addMarker(event.latLng);
+        map.addListener("click", event => {
+            alrt()
+            addMarker(event.latLng);
+        });
+        function addMarker(location, value) {
+        const marker = new google.maps.Marker({
+          position: location,
+          map: map,
+          title: value,
+          label: labels[labelIndex++ % labels.length],
+          map: map,
       });
-      addMarker(haightAshbury);
-    }
 
-    function addMarker(location, value) {
-      const marker = new google.maps.Marker({
-        position: location,
-        map: map,
-        title: value,
-      });
-      markers.push(marker);
-      console.log(value, "1")
+      markers[i].push(marker);
+      console.log(marker.label)
+      for (var i = 0; i < markers.length; i++) {
+        var mark = markers[i]
+        console.log(mark);
+      }
     }
-    initMap()
+    function alrt(value){
+          var value = prompt("Please enter your text");
+          if (value == null || value == "") {
+          alert("Your have added marker with out text"),alrt();
+          } 
+      console.log(typeof value)
+      console.log(markers)
+      }
+
+    }
+    
+    google.maps.event.addDomListener(window, 'load', initialize);
+
 })
 
 
 
+  
