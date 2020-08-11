473cf3b6ac9c1dec1917aa7cd915881a9d898112
Aleksei <kilroygood@gmail.com>
2020-08-11 15:57:50 +0300

    array

--- script.js
+++ script.js
@@ -6,46 +6,44 @@ console.log("DOM fully loaded and parsed");
     let labelIndex = 0;
 
 function save() {
-  localStorage.setItem('markers', JSON.stringify(locations));
-  locations = JSON.parse(localStorage.getItem('markers'));
+  localStorage.setItem('markers', JSON.stringify(markers))
 }
+save()
 
-    console.log(markers)
+    console.log(typeof markers)
 
     function initialize() {
+
+      for (var i = 0; i < markers.length; i++) {
+        console.log(markers[i]);
+      }
+
         var mapOptions = {
             zoom: 18,
             center: new google.maps.LatLng(38.983064, -76.9473872) 
         };
     
         var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+
     
-        markers[0] = new google.maps.Marker({
-            position: new google.maps.LatLng(38.983064, -76.9473872),
-            map: map,
-            label: labels[labelIndex++ % labels.length],
-            title: "Van Munching Hall"
-        });
-    
-        markers[1] = new google.maps.Marker({
+        const marker = new google.maps.Marker({
             position: new google.maps.LatLng(38.9827089, -76.9475412),
             map: map,
             label: labels[labelIndex++ % labels.length],
             title: "The Smith Store",
         });
-    
-
         for (var i = 0; i < markers.length; i++) {
-          
-          var mark = markers[i]
           console.log(mark);
         }
+        markers[i].push(marker);
+        console.log(marker)
+        save()
         
-        google.maps.event.addListener(mark, 'click', removeMar) 
+        google.maps.event.addListener(markers[i], 'click', removeMar) 
           
         
         function removeMar() {
-          console.log("sd")
+          console.log("remove")
           for (var i = 1; i < markers.length; i++)
           markers[i].setMap(null);
           save()
@@ -62,12 +60,12 @@ function save() {
           label: labels[labelIndex++ % labels.length],
           map: map,
       });
-      markers[i].push(marker);
-      console.log(marker.label)
       for (var i = 0; i < markers.length; i++) {
         var mark = markers[i]
         console.log(mark);
       }
+      markers[i].push(marker);
+      console.log(marker)
       save()
     }
     function alrt(value){
@@ -80,7 +78,6 @@ function save() {
       }
 
     }
-    
     google.maps.event.addDomListener(window, 'load', initialize);
 
 })
